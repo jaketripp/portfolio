0c7574cff1b9fd25a742796ea55ec2b593c898f4
Jacob Tripp <jake@shimmer.home>
2018-02-08 02:13:53 -0600

    rename AppInfo component to AppInfoPage

---
+++ src/components/AppInfoPage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Footer from './Footer';
+import Header from './Header';
+import NotFoundPage from './NotFoundPage';
+
+const projectNames = ['asoftoday', 'color-conversion', 'expensify'];
+
+const AppInfo = (props) => {
+    const appName = props.match.params.name;
+    if (projectNames.includes(appName)) {
+        return (
+            <div>
+                <Header />
+                This is the App Info page
+                <Link to="/">Go home</Link>
+                <Footer />
+            </div>
+        );
+    } else {
+        return (
+            <NotFoundPage />
+        )
+    }
+}
+
+export default AppInfo;
