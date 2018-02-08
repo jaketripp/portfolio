c6d97167c1fcb5884c60c95631efd2e33fc00015
Jacob Tripp <jake@shimmer.home>
2018-02-07 20:12:02 -0600

    Minor fix to 404 page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Header from './Header';
+import Footer from './Footer';
+
 const NotFoundPage = () => (
-  <div>
-    404 - <Link to="/">Go home</Link>
-  </div>
+	<div>
+		<Header />
+		<div className="content-container">
+			<div className="not-found-page">
+				<h1>404 - <Link to="/">Go home</Link></h1>
+			</div>
+		</div>
+		<Footer />
+	</div>
 );
 
 export default NotFoundPage;
