e3b8599715c1095b44320bf4e4783dbe5f18b421
Jacob Tripp <jake@shimmer.home>
2018-02-07 18:14:18 -0600

    Add Bio component

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
+import Bio from './Bio';
 
 const DashboardPage = () => (
   <div>
     <Header />
+    <Bio />
     <Footer />
   </div>
 );
