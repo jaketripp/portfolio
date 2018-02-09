f04ff75ef369bbb6f918c04e3e511b6b0a721b65
Jacob Tripp <jake@shimmer.home>
2018-02-09 15:18:30 -0600

    Break projectList out into it's own file

---
+++ src/data/projectList.js
@@ -0,0 +1,103 @@
+const projectList = [
+    {
+        backgroundColor: '#184860',
+        color: '#ffffff',
+        title: 'As of Today',
+        logo: 'http://jaketripp.com/images/asoftoday.jpg',
+        demoURL: 'http://asoftoday.com', // 'https://jaketripp.com/apps/asoftoday'
+        infoURLName: 'asoftoday',
+        githubURL: 'https://github.com/jaketripp/asoftoday',
+    },
+    {
+        backgroundColor: '#F4883C',
+        color: '#ffffff',
+        title: 'Seasonal',
+        logo: 'http://jaketripp.com/images/seasonal.jpg',
+        demoURL: 'https://jaketripp.github.io/Seasonal',
+        infoURLName: 'seasonal',
+        githubURL: 'https://github.com/jaketripp/Seasonal',
+    },
+    {
+        backgroundColor: '#184860',
+        color: '#ffffff',
+        title: 'As of Today',
+        logo: 'http://jaketripp.com/images/asoftoday.jpg',
+        demoURL: 'http://asoftoday.com', // 'https://jaketripp.com/apps/asoftoday'
+        infoURLName: 'asoftoday',
+        githubURL: 'https://github.com/jaketripp/asoftoday',
+    },
+    {
+        backgroundColor: '#F4883C',
+        color: '#ffffff',
+        title: 'Seasonal',
+        logo: 'http://jaketripp.com/images/seasonal.jpg',
+        demoURL: 'https://jaketripp.github.io/Seasonal',
+        infoURLName: 'seasonal',
+        githubURL: 'https://github.com/jaketripp/Seasonal',
+    },
+    {
+        backgroundColor: '#184860',
+        color: '#ffffff',
+        title: 'As of Today',
+        logo: 'http://jaketripp.com/images/asoftoday.jpg',
+        demoURL: 'http://asoftoday.com', // 'https://jaketripp.com/apps/asoftoday'
+        infoURLName: 'asoftoday',
+        githubURL: 'https://github.com/jaketripp/asoftoday',
+    },
+    {
+        backgroundColor: '#F4883C',
+        color: '#ffffff',
+        title: 'Seasonal',
+        logo: 'http://jaketripp.com/images/seasonal.jpg',
+        demoURL: 'https://jaketripp.github.io/Seasonal',
+        infoURLName: 'seasonal',
+        githubURL: 'https://github.com/jaketripp/Seasonal',
+    },
+    {
+        backgroundColor: '#184860',
+        color: '#ffffff',
+        title: 'As of Today',
+        logo: 'http://jaketripp.com/images/asoftoday.jpg',
+        demoURL: 'http://asoftoday.com', // 'https://jaketripp.com/apps/asoftoday'
+        infoURLName: 'asoftoday',
+        githubURL: 'https://github.com/jaketripp/asoftoday',
+    },
+    {
+        backgroundColor: '#F4883C',
+        color: '#ffffff',
+        title: 'Seasonal',
+        logo: 'http://jaketripp.com/images/seasonal.jpg',
+        demoURL: 'https://jaketripp.github.io/Seasonal',
+        infoURLName: 'seasonal',
+        githubURL: 'https://github.com/jaketripp/Seasonal',
+    },
+    {
+        backgroundColor: '#184860',
+        color: '#ffffff',
+        title: 'As of Today',
+        logo: 'http://jaketripp.com/images/asoftoday.jpg',
+        demoURL: 'http://asoftoday.com', // 'https://jaketripp.com/apps/asoftoday'
+        infoURLName: 'asoftoday',
+        githubURL: 'https://github.com/jaketripp/asoftoday',
+    },
+    {
+        backgroundColor: '#F4883C',
+        color: '#ffffff',
+        title: 'Seasonal',
+        logo: 'http://jaketripp.com/images/seasonal.jpg',
+        demoURL: 'https://jaketripp.github.io/Seasonal',
+        infoURLName: 'seasonal',
+        githubURL: 'https://github.com/jaketripp/Seasonal',
+    },
+    {
+        backgroundColor: '#184860',
+        color: '#ffffff',
+        title: 'As of Today',
+        logo: 'http://jaketripp.com/images/asoftoday.jpg',
+        demoURL: 'http://asoftoday.com', // 'https://jaketripp.com/apps/asoftoday'
+        infoURLName: 'asoftoday',
+        githubURL: 'https://github.com/jaketripp/asoftoday',
+    }
+];
+
+module.exports = { projectList };
