b6d95332cef8b453166d84bdd2426cf913f88b9e
Jacob Tripp <jake@shimmer.home>
2018-02-09 02:01:39 -0600

    Make button hover work dynamically using radium

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,48 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Radium from 'radium';
+const RadiumLink = Radium(Link);
 
-export const Card = (props) => {
-    const cardStyle = {
-        color: props.data.color,
-    };
-    const overlayStyle = {
-        backgroundColor: props.data.backgroundColor
-    };
-    return (
-        <div className="card" style={cardStyle}>
-            <img src={props.data.logo} alt={`${props.data.title} logo`} className="image" />
+class Card extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ...props
+        }
+    }
+
+    render() {
+        const cardStyle = {
+            color: this.state.data.color
+        }
+        const overlayStyle = {
+            background: this.state.data.backgroundColor
+        }
+
+        const btnStyle = {
+            background: this.state.data.backgroundColor,
+            color: this.state.data.color,
+            border: `2px solid ${this.state.data.color}`,
+
+            ':hover': {
+                background: this.state.data.color,
+                color: this.state.data.backgroundColor
+            },
+        };
+
+        return (
+            <div className="card" style={cardStyle}>
+                <img src={this.state.data.logo} alt={`${this.state.data.title} logo`} className="image" />
                 <div className="overlay" style={overlayStyle}>
-                    <h3>{props.data.title}</h3>
+                    <h3>{this.state.data.title}</h3>
                     <div className="text">
-                        <Link className="card__links__button" to={props.data.demoURL}>Demo</Link>
-                        <Link className="card__links__button" to={props.data.githubURL}>GitHub</Link>
-                        <Link className="card__links__button" to={`/apps/info/${props.data.infoURLName}`}>More Info</Link>
+                        <RadiumLink className="card__links__button" style={btnStyle} to={this.state.data.demoURL}>Demo</RadiumLink>
+                        <RadiumLink className="card__links__button" style={btnStyle} to={this.state.data.githubURL}>GitHub</RadiumLink>
+                        <RadiumLink className="card__links__button" style={btnStyle} to={`/apps/info/${this.state.data.infoURLName}`}>More Info</RadiumLink>
                     </div>
                 </div>
-        </div>
-                
-    );
-}
+            </div>
 
-export default Card;
+        );
+    }
+};
 
-// var Link = React.createClass({
-//     getInitialState: function () {
-//         return { hover: false }
-//     },
-//     toggleHover: function () {
-//         this.setState({ hover: !this.state.hover })
-//     },
-//     render: function () {
-//         var linkStyle;
-//         if (this.state.hover) {
-//             linkStyle = { backgroundColor: 'red' }
-//         } else {
-//             linkStyle = { backgroundColor: 'blue' }
-//         }
-//         return (
-//             <div>
-//                 <a style={linkStyle} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}>Link</a>
-//             </div>
-//         )
-//     }
+export default Radium(Card);
