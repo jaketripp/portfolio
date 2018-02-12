a5efc005dd0a49376aac6e1df37f3d9029c7b313
Jacob Tripp <jake@shimmer.home>
2018-02-11 23:51:59 -0600

    Combine all data into one file (a giant obj)

---
+++ src/data/apps.js
@@ -0,0 +1,280 @@
+const apps = {
+    'musicmaster': {
+        title: 'Music Master',
+        style: {
+            color: '#ffffff',
+            backgroundColor: '#2c5b61'
+        },
+        logo: '/images/cards/music-master.jpg',
+        demoURL: 'https://jaketripp-music-master.herokuapp.com/',
+        infoURLName: 'musicmaster',
+        githubURL: 'https://github.com/jaketripp/music-master',
+        tech: [],
+        screenshot: '/images/screenshots/music-master-screenshot.png',
+        description: '',
+        why: '',
+        coolFeature: '',
+        date: ''
+    },
+    'expensify': {
+        title: 'Expensify',
+        style: {
+            color: '#ffffff',
+            backgroundColor: '#1890c0'
+        },
+        logo: '/images/cards/expensify.jpg',
+        demoURL: 'https://jaketripp-expensify.herokuapp.com/',
+        infoURLName: 'expensify',
+        githubURL: 'https://github.com/jaketripp/expensify',
+        tech: [],
+        screenshot: '',
+        description: '',
+        why: '',
+        coolFeature: '',
+        date: ''
+    },
+    'hairbyfrancisco': {
+        title: 'Hair by Francisco',
+        style: {
+            color: '#ffffff',
+            backgroundColor: '#7D5DB2'
+        },
+        logo: '/images/cards/hf.png',
+        demoURL: 'http://hairbyfrancisco.com',
+        infoURLName: 'hairbyfrancisco',
+        githubURL: 'https://github.com/jaketripp/hairByFrancisco/',
+        tech: [],
+        screenshot: '',
+        description: '',
+        why: '',
+        coolFeature: '',
+        date: ''
+    },
+    'asoftoday': {
+        title: 'As of Today',
+        style: {
+            color: '#ffffff',
+            backgroundColor: '#184860'
+        },
+        logo: '/images/cards/asoftoday.jpg',
+        demoURL: 'http://asoftoday.me', // 'https://jaketripp.com/apps/asoftoday'
+        infoURLName: 'asoftoday',
+        githubURL: 'https://github.com/jaketripp/asoftoday',
+        tech: [],
+        screenshot: '',
+        description: '',
+        why: '',
+        coolFeature: '',
+        date: ''
+    },
+    'sacc': {
+        title: 'SA Coding Challenge',
+        style: {
+            color: '#ffffff',
+            backgroundColor: '#181818'
+        },
+        logo: '/images/cards/sacc.jpg',
+        demoURL: 'http://sacodingchallenge.com',
+        infoURLName: 'sacc',
+        githubURL: 'https://github.com/sacodingchallenge/sacodingchallenge',
+        tech: [],
+        screenshot: '',
+        description: '',
+        why: '',
+        coolFeature: '',
+        date: ''
+    },
+    'countdownchamp': {
+        title: 'Countdown Champ',
+        style: {
+            color: '#ffffff',
+            backgroundColor: '#476D36'
+        },
+        logo: '/images/cards/clock.jpg',
+        demoURL: 'https://jaketripp-countdown-champ.herokuapp.com/',
+        infoURLName: 'countdownchamp',
+        githubURL: 'https://github.com/jaketripp/countdown-champ',
+        tech: [],
+        screenshot: '',
+        description: '',
+        why: '',
+        coolFeature: '',
+        date: ''
+    },
+    'chat': {
+        title: 'Chat',
+        style: {
+            color: '#ffffff',
+            backgroundColor: '#78a8d8'
+        },
+        logo: '/images/cards/chat.jpg',
+        demoURL: 'http://jaketripp-chat-app.herokuapp.com/',
+        infoURLName: 'chat',
+        githubURL: 'https://github.com/jaketripp/node-chat-app',
+        tech: [],
+        screenshot: '',
+        description: '',
+        why: '',
+        coolFeature: '',
+        date: ''
+    },
+    'simon': {
+        title: 'Simon Game',
+        style: {
+            color: '#FFFFFF',
+            backgroundColor: '#CFC977'
+        },
+        logo: '/images/cards/simon.png',
+        demoURL: 'https://jaketripp.github.io/simon/',
+        infoURLName: 'simon',
+        githubURL: 'https://github.com/jaketripp/simon/',
+        tech: [],
+        screenshot: '',
+        description: '',
+        why: '',
+        coolFeature: '',
+        date: ''
+    },
+    'yelpcamp': {
+        title: 'YelpCamp',
+        style: {
+            color: '#ffffff',
+            backgroundColor: '#304848'
+        },
+        logo: '/images/cards/yelpcamp.jpg',
+        demoURL: 'https://jaketripp-yelp-camp.herokuapp.com/',
+        infoURLName: 'yelpcamp',
+        githubURL: 'https://github.com/jaketripp/yelpcamp',
+        tech: [],
+        screenshot: '',
+        description: '',
+        why: '',
+        coolFeature: '',
+        date: ''
+    },
+    'calculator': {
+        title: 'Calculator',
+        style: {
+            color: '#ffffff',
+            backgroundColor: '#1890d8'
+        },
+        logo: '/images/cards/calculator.png',
+        demoURL: 'https://jaketripp.github.io/calculator/',
+        infoURLName: 'calculator',
+        githubURL: 'https://github.com/jaketripp/calculator/',
+        tech: [],
+        screenshot: '',
+        description: '',
+        why: '',
+        coolFeature: '',
+        date: ''
+    },
+    'seasonal': {
+        title: 'Seasonal',
+        style: {
+            color: '#ffffff',
+            backgroundColor: '#F4883C'
+        },
+        logo: '/images/cards/seasonal.jpg',
+        demoURL: 'https://jaketripp.github.io/Seasonal',
+        infoURLName: 'seasonal',
+        githubURL: 'https://github.com/jaketripp/Seasonal',
+        tech: [],
+        screenshot: '',
+        description: '',
+        why: '',
+        coolFeature: '',
+        date: ''
+    },
+    'tictactoe': {
+        title: 'Tic Tac Toe',
+        style: {
+            color: '#ffffff',
+            backgroundColor: '#00A717'
+        },
+        logo: '/images/cards/tictactoe.png',
+        demoURL: 'https://jaketripp.github.io/tictactoe/',
+        infoURLName: 'tictactoe',
+        githubURL: 'https://github.com/jaketripp/tictactoe/',
+        tech: [],
+        screenshot: '',
+        description: '',
+        why: '',
+        coolFeature: '',
+        date: ''
+    },
+    'colorconversion': {
+        title: 'Color Conversion',
+        style: {
+            color: '#FFFFFF',
+            backgroundColor: '#a81890'
+        },
+        logo: '/images/cards/color.jpg',
+        demoURL: 'https://jaketripp.github.io/Color-Conversion/',
+        infoURLName: 'colorconversion',
+        githubURL: 'https://github.com/jaketripp/Color-Conversion/',
+        tech: [],
+        screenshot: '',
+        description: '',
+        why: '',
+        coolFeature: '',
+        date: ''
+    },
+    'piglatinconverter': {
+        title: 'Pig Latin Converter',
+        style: {
+            color: '#ffffff',
+            backgroundColor: '#f0a890'
+        },
+        logo: '/images/cards/pig.jpg',
+        demoURL: 'https://jaketripp.github.io/PigLatinConverter/',
+        infoURLName: 'piglatinconverter',
+        githubURL: 'https://github.com/jaketripp/PigLatinConverter',
+        tech: [],
+        screenshot: '',
+        description: '',
+        why: '',
+        coolFeature: '',
+        date: ''
+    },
+    'wikiviewer': {
+        title: 'Wiki Viewer',
+        style: {
+            color: '#ffffff',
+            backgroundColor: '#f07818'
+        },
+        logo: '/images/cards/wikiviewer.png',
+        demoURL: 'https://jaketripp.github.io/wikiviewer/',
+        infoURLName: 'wikiviewer',
+        githubURL: 'https://github.com/jaketripp/wikiviewer/',
+        tech: [],
+        screenshot: '',
+        description: '',
+        why: '',
+        coolFeature: '',
+        date: ''
+    },
+    'kokolunchbox': {
+        title: 'Koko Lunchbox',
+        style: {
+            color: '#ffffff',
+            backgroundColor: '#c00030'
+        },
+        logo: '/images/cards/koko.png',
+        demoURL: 'https://jaketripp.github.io/Koko-Lunchbox/',
+        infoURLName: 'kokolunchbox',
+        githubURL: 'https://github.com/jaketripp/Koko-Lunchbox',
+        tech: [],
+        screenshot: '',
+        description: '',
+        why: '',
+        coolFeature: '',
+        date: ''
+    }
+};
+
+module.exports = { apps };
+
+// chat
+// include this on page
+// <a href="https://www.freepik.com/free-vector/chat-speech-bubble-icon_761194.htm">Designed by Freepik</a>
