470a13d564dad4ecb70fe5460fa80476f38f335d
Jacob Tripp <jake@shimmer.home>
2018-02-11 23:58:40 -0600

    explicitly return jsx

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export const Header = () => (
-	<header className="header">
-		<div className="content-container">
-			<div className="header__content">
-				<Link className="header__title" to="/">
-					<h1>Jake Tripp</h1>
-					<h3>Full-Stack Developer</h3>
-				</Link>
+export const Header = () => {
+	return (
+		<header className="header">
+			<div className="content-container">
+				<div className="header__content">
+					<Link className="header__title" to="/">
+						<h1>Jake Tripp</h1>
+						<h3>Full Stack Developer</h3>
+					</Link>
+				</div>
 			</div>
-		</div>
-	</header>
-);
+		</header>
+	);
+}
 
 export default Header;
