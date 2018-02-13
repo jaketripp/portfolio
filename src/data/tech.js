59a070bae40fa0ea8ea7037b38e34a4855208e2d
Jacob Tripp <jake@shimmer.home>
2018-02-12 20:31:49 -0600

    Add tech icons and provide basic styling

---
+++ src/data/tech.js
@@ -0,0 +1,92 @@
+const techObj = {
+    'webpack': {
+        logo: '/images/tech/webpack.svg',
+        name: 'webpack'
+    },
+    'mongodb': {
+        logo: '/images/tech/mongodb.svg',
+        name: 'mongodb'
+    },
+    'firebase': {
+        logo: '/images/tech/firebase.svg',
+        name: 'firebase'
+    },
+    'node': {
+        logo: '/images/tech/node.svg',
+        name: 'node'
+    },
+    'npm': {
+        logo: '/images/tech/npm.svg',
+        name: 'npm'
+    },
+    'oauth': {
+        logo: '/images/tech/oauth.svg',
+        name: 'oauth'
+    },
+    'express': {
+        logo: '/images/tech/express.svg',
+        name: 'express'
+    },
+    'react': {
+        logo: '/images/tech/react.svg',
+        name: 'react'
+    },
+    'redux': {
+        logo: '/images/tech/redux.svg',
+        name: 'redux'
+    },
+    'react-router': {
+        logo: '/images/tech/react-router.svg',
+        name: 'react-router'
+    },
+    'es6': {
+        logo: '/images/tech/es6.svg',
+        name: 'es6'
+    },
+    'sass': {
+        logo: '/images/tech/sass.svg',
+        name: 'sass'
+    },
+    'babel': {
+        logo: '/images/tech/babel.svg',
+        name: 'babel'
+    },
+    'socketio': {
+        logo: '/images/tech/socketio.svg',
+        name: 'socketio'
+    },
+    'css-3': {
+        logo: '/images/tech/css-3.svg',
+        name: 'css-3'
+    },
+    'javascript': {
+        logo: '/images/tech/javascript.svg',
+        name: 'javascript'
+    },
+    'html-5': {
+        logo: '/images/tech/html-5.svg',
+        name: 'html-5'
+    },
+    'jest': {
+        logo: '/images/tech/jest.svg',
+        name: 'jest'
+    },
+    'momentjs': {
+        logo: '/images/tech/momentjs.svg',
+        name: 'momentjs'
+    },
+    'mailchimp': {
+        logo: '/images/tech/mailchimp.svg',
+        name: 'mailchimp'
+    },
+    'semantic-ui': {
+        logo: '/images/tech/semantic-ui.svg',
+        name: 'semantic-ui'
+    },
+    'bootstrap': {
+        logo: '/images/tech/bootstrap.svg',
+        name: 'bootstrap'
+    }
+};
+
+module.exports = { techObj };
