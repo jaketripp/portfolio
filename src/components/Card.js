19f8eca4afc1cbf62c3ac2f52ad02a4c78f6f479
Jacob Tripp <jake@shimmer.home>
2018-02-14 22:01:34 -0600

    Go back to working commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,36 +1,76 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Overlay from './Overlay';
+import Radium from 'radium';
+const RadiumLink = Radium(Link);
 
 class Card extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isHovering: false,
+            blockStyle: {
+                'display': 'block'
+            },
             ...props
         }
     }
 
-    handleMouseHover = () => {
+    restoreBlock() {
         this.setState({
-            isHovering: !this.state.isHovering,
+            blockStyle: {
+                'display': 'block'
+            }
         });
+    }
+
+    removeBlock() {
+        setTimeout(() => {
+            this.setState({
+                blockStyle: {
+                    'display': 'none'
+                }
+            });
+        }, 10);
     }
 
     render() {
         const cardStyle = {
             color: this.state.data.style.color
         }
+        const overlayStyle = {
+            background: this.state.data.style.backgroundColor
+        }
 
+        const btnStyle = {
+            background: this.state.data.style.backgroundColor,
+            color: this.state.data.style.color,
+            border: `2px solid ${this.state.data.style.color}`,
+
+            ':hover': {
+                background: this.state.data.style.color,
+                color: this.state.data.style.backgroundColor
+            },
+        };
+
+        // block buttons until 10 ms after mouse has entered card
+        // block buttons on mouseLeave
         return (
-            <div className="card" style={cardStyle} onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}>
+            <div className="card" style={cardStyle} onMouseEnter={(e) => this.removeBlock(e)} onMouseLeave={() => this.restoreBlock()}>
                 <img src={this.state.data.logo} alt={`${this.state.data.title} logo`} className="image" />
-                {this.state.isHovering && <Overlay {...this.state} />}
-                
+                <div className="overlay" style={overlayStyle}>
+                    <h3>{this.state.data.title}</h3>
+                    <div className="text">
+
+                        <div className="block" style={this.state.blockStyle}></div>
+
+                        <a className="card__links__button" style={btnStyle} key={this.state.data.demoURL} href={this.state.data.demoURL}>Demo</a>
+                        <a className="card__links__button" style={btnStyle} key={this.state.data.githubURL} href={this.state.data.githubURL}>GitHub</a>
+                        <RadiumLink className="card__links__button" style={btnStyle} to={`/about/${this.state.data.infoURLName}`}>More Info</RadiumLink>
+                    </div>
+                </div>
             </div>
+
         );
     }
 };
 
-export default Card;
-
+export default Radium(Card);
