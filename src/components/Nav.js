8d24c5778207a4340a520471bfab5e873931b365
Jacob Tripp <jake@shimmer.home>
2018-02-18 15:21:07 -0600

    Replace Header with Nav

---
+++ src/components/Nav.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Nav extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collapsed: true
+        };
+    }
+
+    toggleNavbar = () => {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
+    render() {
+        return (
+            <div className="navbar">
+                <div className="content-container">
+                    <div className="navbar__content">
+                        <div className="navbar-brand">
+                            <Link className="navbar__title" to="/">
+                                <h1>Jake Tripp</h1>
+                            </Link>
+                        </div>
+                        <div className="navbar-links">
+                            <Link to="/">Home</Link>
+                            <Link to="/">About</Link>
+                            <Link to="/">Work</Link>
+                            <Link to="/contact">Contact</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+// <div>
+//     <Navbar color="faded" light>
+//         <NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
+//         <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+//         <Collapse isOpen={!this.state.collapsed} navbar>
+//             <Nav navbar>
+//                 <NavItem>
+//                     <NavLink href="/components/">Components</NavLink>
+//                 </NavItem>
+//                 <NavItem>
+//                     <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+//                 </NavItem>
+//             </Nav>
+//         </Collapse>
+//     </Navbar>
+// </div>
