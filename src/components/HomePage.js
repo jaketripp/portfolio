22740c550f7f150b7982ae73e6d1962ee7cd283f
Jacob Tripp <jake@shimmer.home>
2018-02-18 20:06:42 -0600

    Add About component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Footer from './Footer';
 import Nav from './Nav';
 import Bio from './Bio';
+import About from './About';
 import CardList from './CardList';
 
 const HomePage = () => (
   <div>
     <Nav />
     <Bio />
+    <About />
     <CardList />
     <Footer />
   </div>
