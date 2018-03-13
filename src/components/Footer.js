f6b4b196e4db3171f3ba8f890c20602c05d74489
Jacob Tripp <jake@shimmer.home>
2018-03-13 02:28:23 -0500

    Add envelope icon and email, add titles for icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,18 +3,19 @@ const FacebookLogo = require('react-icons/lib/fa/facebook');
 const LinkedInLogo = require('react-icons/lib/fa/linkedin');
 const OldMediumLogo = require('react-icons/lib/fa/medium');
 const GitHubLogo = require('react-icons/lib/fa/github');
+const Envelope = require('react-icons/lib/fa/envelope');
 
 const Footer = () => (
     <div className="footer" id="contact">
         <div className="footer__content">
             <div className="footer__icons">
-                <a href="https://github.com/jaketripp" aria-label="Github link"><GitHubLogo /></a>
-                <a href="https://www.linkedin.com/in/jake-tripp/" aria-label="LinkedIn link"><LinkedInLogo /></a>
-                <a href="https://medium.com/@jaketripp" aria-label="Medium link"><OldMediumLogo /></a>
-                <a href="https://www.facebook.com/jaketripp95" aria-label="Facebook link"><FacebookLogo /></a>
+                <a href="https://github.com/jaketripp" aria-label="GitHub link" title="GitHub"><GitHubLogo /></a>
+                <a href="https://www.linkedin.com/in/jake-tripp/" aria-label="LinkedIn link" title="LinkedIn"><LinkedInLogo /></a>
+                <a href="https://medium.com/@jaketripp" aria-label="Medium link" title="Medium"><OldMediumLogo /></a>
+                <a href="https://www.facebook.com/jaketripp95" aria-label="Facebook link" title="Facebook"><FacebookLogo /></a>
+                <a href="mailto:jake@jaketripp.com" aria-label="Email link" title="jake@jaketripp.com"><Envelope /></a>
             </div>
             <div className="footer__copyright">
-                <p><a href="mailto:jake@jaketripp.com">jake@jaketripp.com</a></p>
                 <p>Jake Tripp &copy; 2018</p>
             </div>
         </div>
