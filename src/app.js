9a8d7aeade345fcab3fd2f5e5852d7ab83e4b476
Jacob Tripp <jake@shimmer.home>
2018-03-15 00:10:32 -0500

    Remove reference to old import statement

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import AppRouter, { history } from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import 'react-dates/lib/css/_datepicker.css';
 import LoadingPage from './components/LoadingPage';
 
 const jsx = (
