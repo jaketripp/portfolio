7fb38bad67cf0d1d286eb696511c0b1d717f4e0f
Jacob Tripp <jake@shimmer.home>
2018-05-22 18:45:11 -0500

    Add Java to skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,7 +5,7 @@ export const Skills = () => (
         <div className="content-container">
             <h3>skills</h3>
             <div className="skills__content">
-                <p><strong>Languages:</strong> JavaScript, some Python, a little C</p>
+                <p><strong>Languages:</strong> JavaScript, some Python, currently learning Java</p>
                 <p><strong>Libraries &amp; Frameworks:</strong> React, Redux, Node.js, Express, Webpack, Babel, SCSS, jQuery, Semantic-UI, Bootstrap</p>
                 <p><strong>Databases:</strong> MongoDB, Firebase</p>
                 <p><strong>Testing:</strong> Mocha, Chai, Selenium, WebdriverIO, Jest</p>
