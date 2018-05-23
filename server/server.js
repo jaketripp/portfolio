e18dc63ea3687a803e9eadc56fd48d9718d6d1b4
Jacob Tripp <jake@shimmer.home>
2018-05-22 23:02:26 -0500

    Enable Express compression

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const path = require('path');
 const express = require('express');
+const compression = require('compression');
 const app = express();
+
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
+
+app.use(compression());
 
 app.use(express.static(publicPath));
 
