4874f0bcbdad9042c02e0470d9efbe31ed099c10
Jacob Tripp <jake@shimmer.home>
2018-05-23 02:09:51 -0500

    Change port to 8080 because king nginx says so

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const compression = require('compression');
 const app = express();
 
 const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(compression());
 
