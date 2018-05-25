b5c40ca4d6695cb4de769002246ce90c106ff185
Jacob Tripp <jake@shimmer.home>
2018-05-25 18:28:06 -0500

    Add blurb about Shimberg Center

--- src/components/About.js
+++ src/components/About.js
@@ -8,6 +8,7 @@ export const About = () => (
                 <p>I love to understand the way things work. Everything - from simple kitchen gadgets to complex calculus equations. Understanding helps equip me to create. And <strong>I <em>love</em> to create.</strong></p>
                 <p>My passion is making <strong>responsive, mobile-first web applications and websites.</strong></p>
                 <p>When I’m not clickin' away at my keyboard, you’ll find me playing the guitar, reading, or cooking.</p>
+                <p>I currently work as the lead web developer for the Shimberg Center, a department of the University of Florida aimed at promoting "safe, decent and affordable housing and related community development throughout the state of Florida."</p>
             </div>
         </div>
     </div>
