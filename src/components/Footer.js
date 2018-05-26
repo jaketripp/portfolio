a14d244088a89e53a287c0c4def6284a6fe85069
Jacob Tripp <jake@shimmer.home>
2018-05-26 00:20:31 -0500

    Make footer taller, more substantial, add contact header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,17 +6,20 @@ const GitHubLogo = require('react-icons/lib/fa/github');
 const Envelope = require('react-icons/lib/fa/envelope');
 
 const Footer = () => (
-    <div className="footer" id="contact">
-        <div className="footer__content">
-            <div className="footer__icons">
-                <a href="https://github.com/jaketripp" aria-label="GitHub link" title="GitHub"><GitHubLogo /></a>
-                <a href="https://www.linkedin.com/in/jake-tripp/" aria-label="LinkedIn link" title="LinkedIn"><LinkedInLogo /></a>
-                <a href="https://medium.com/@jaketripp" aria-label="Medium link" title="Medium"><OldMediumLogo /></a>
-                <a href="https://www.facebook.com/jaketripp95" aria-label="Facebook link" title="Facebook"><FacebookLogo /></a>
-                <a href="mailto:jake@jaketripp.com" aria-label="Email link" title="jake@jaketripp.com"><Envelope /></a>
-            </div>
-            <div className="footer__copyright">
-                <p>Jake Tripp &copy; 2018</p>
+    <div className="section section__secondary footer" id="contact">
+        <div className="content-container">
+            <h3>contact</h3>
+            <div className="footer__content">
+                <div className="footer__icons">
+                    <a href="https://github.com/jaketripp" aria-label="GitHub link" title="GitHub"><GitHubLogo /></a>
+                    <a href="https://www.linkedin.com/in/jake-tripp/" aria-label="LinkedIn link" title="LinkedIn"><LinkedInLogo /></a>
+                    <a href="https://medium.com/@jaketripp" aria-label="Medium link" title="Medium"><OldMediumLogo /></a>
+                    <a href="https://www.facebook.com/jaketripp95" aria-label="Facebook link" title="Facebook"><FacebookLogo /></a>
+                    <a href="mailto:jake@jaketripp.com" aria-label="Email link" title="jake@jaketripp.com"><Envelope /></a>
+                </div>
+                <div className="footer__copyright">
+                    <p>Jake Tripp &copy; 2018</p>
+                </div>
             </div>
         </div>
     </div>
