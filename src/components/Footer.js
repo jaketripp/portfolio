73a8ef1b73eeaaae15f2ea9c69447943501daa85
Jake <shmogens@gmail.com>
2018-07-20 17:02:35 -0500

    Add Instagram icon and make Instagram and Medium icons custom (script that copies files into node_modules)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ const OldMediumLogo = require('react-icons/lib/fa/medium');
 const GitHubLogo = require('react-icons/lib/fa/github');
 const Envelope = require('react-icons/lib/fa/envelope');
 const SoundCloudLogo = require('react-icons/lib/fa/soundcloud');
+const InstagramLogo = require('react-icons/lib/fa/instagram');
 
 const Footer = () => (
     <div className="section section__secondary footer" id="contact">
@@ -18,6 +19,7 @@ const Footer = () => (
                     <a href="mailto:jake@jaketripp.com" aria-label="Email link" title="jake@jaketripp.com"><Envelope /></a>
                     <a href="https://www.youtube.com/channel/UCGWw6-QNLp_vAIRn1ANGh9g?view_as=subscriber" aria-label="YouTube link" title="YouTube"><YouTubeLogo /></a>
                     <a href="https://soundcloud.com/the-jake-tripp" aria-label="SoundCloud link" title="SoundCloud"><SoundCloudLogo /></a>
+                    <a href="https://www.instagram.com/thejaketripp/" aria-label="Instagram link" title="Instagram"><InstagramLogo /></a>
                 </div>
                 <div className="footer__copyright">
                     <p>Jake Tripp &copy; 2018</p>
