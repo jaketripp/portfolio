f78fa75d809f0506ca14d52edefca747a9eb951e
Jake Tripp <jake@jake.local>
2018-08-23 20:56:56 -0500

    Update about section to be H-E-B

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export const About = () => (
                 <p>I love to understand the way things work. Everything - from simple kitchen gadgets to complex calculus equations. Understanding helps equip me to create. And <strong>I <em>love</em> to create.</strong></p>
                 <p>My passion is making <strong>responsive, mobile-first web applications and websites.</strong></p>
                 <p>When I’m not clickin' away at my keyboard, you’ll find me playing the guitar, reading, or cooking.</p>
-                <p>I currently work as the lead web developer for the Shimberg Center, a department of the University of Florida aimed at promoting "safe, decent and affordable housing and related community development throughout the state of Florida."</p>
+                <p>I currently work as a software developer at H-E-B’s corporate headquarters in San Antonio, Texas. H-E-B, first established in 1905, was ranked <a href="https://stores.org/stores-top-retailers-2018/" target="_blank" rel="noopener">#20 in top USA retailers</a>, <a href="https://www.forbes.com/best-employers/list/2/" target="_blank" rel="noopener">#53 in America’s Best Employers</a>, and <a href="https://www.marketforce.com/2017-market-research-on-americas-favorite-grocery-chains" target="_blank" rel="noopener">#3 in America’s favorite grocery stores</a>.</p>
             </div>
         </div>
     </div>
