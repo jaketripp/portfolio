9fd4eba577e82d8dca170f806ebe42c9a1d8617c
Jake Tripp <jake@jake.local>
2018-09-06 21:17:14 -0500

    Delete tests (from old project - never tested portfolio)

--- src/tests/setupTests.js
+++
@@ -1,6 +0,0 @@
-import Dotenv from 'dotenv';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Dotenv.config({ path: '.env.test' });
-Enzyme.configure({ adapter: new Adapter() });
