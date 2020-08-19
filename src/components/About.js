9566e69a7b7f316d37f4c75133166441d4f585c7
Jake Tripp <shmogens@gmail.com>
2020-08-18 20:21:25 -0500

    Update about section

--- src/components/About.js
+++ src/components/About.js
@@ -6,25 +6,17 @@ const About = () => (
       <h3>about</h3>
       <div className="about__content">
         <p>
-          I love to understand the way things work. Everything - from simple
+          I love to understand the way things work—everything, from simple
           kitchen gadgets to complex calculus equations. Understanding helps
-          equip me to create. And{" "}
-          <strong>
-            I <em>love</em> to create.
-          </strong>
+          equip me to create. And <strong>I <em>love</em> to create.</strong>
         </p>
         <p>
-          My passion is making{" "}
-          <strong>
-            responsive, mobile-first web applications and websites.
-          </strong>
+          When I'm not clickin' away at my keyboard, you'll find me playing
+          music, going for walks, or cooking.
         </p>
         <p>
-          When I’m not clickin' away at my keyboard, you’ll find me playing the
-          guitar, reading, or cooking.
-        </p>
-        <p>
-          I work remotely as a software engineer at <strong>Amazon</strong> on the Modeling & Optimization team.
+          I work remotely as a software engineer at <strong>Amazon</strong> on the Modeling &
+          Optimization team.
         </p>
       </div>
     </div>
